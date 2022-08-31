c402ef6d67387285abfe824fe85f5f22a7abb0ce
Mauco <maucopozo23@gmail.com>
2022-08-30 21:15:59 -0300

    desafio2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+const App = () => {
+  return (
+   <header>
+    <div>
+      <h1>MUNDIALITO</h1>
+    </div>
+    <div>
+      <ul>
+
+        <li> <a href="index.html">TODO</a></li>
+        <li> <a href="index.html">PAISES</a></li>
+        <li> <a href="index.html">JUGADORES</a></li>
+        <li> <a href="index.html">ESTADIOS</a></li>
+
+      </ul>
+    </div>
+
+   </header>
+  )
+
+}
+
+export default App;
